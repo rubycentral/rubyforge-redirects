8936f51d9860b004ca5ec7c80953cbd28a0f3637
Kerrick Long <me@kerricklong.com>
2025-11-14 08:58:04 -0600

    Add redirects from 2025-05-09-initial-log-analysis.md
    Many thanks to the Internet Archive's Wayback Machine!
    
    Notes:
    
    - For projects that were retired/archived, I preferred redirecting to the announcement (if it was linked from the GitHub repository), otherwise falling back to the archived repository instead.
    - URLs that begin with `http` (intsead of `https`) were only preserved if an `https` version was not available.

--- test/fastly-config-test.rb
+++ test/fastly-config-test.rb
@@ -18,6 +18,108 @@ class TestFastlyConfiguration < Minitest::Test
     assert_equal "http://www.#{DOMAIN}/", response['Location']
   end
 
+  def test_sequel
+    response = request "http://sequel.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://sequel.jeremyevans.net", response['Location']
+  end
+
+  def test_rack
+    response = request "http://rack.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://rack.github.io/rack/", response['Location']
+  end
+
+  def test_rake
+    response = request "http://rake.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://ruby.github.io/rake/", response['Location']
+  end
+
+  def test_wxruby
+    response = request "http://wxruby.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://github.com/mcorino/wxRuby3/wiki", response['Location']
+  end
+
+  def test_amazon
+    response = request "http://amazon.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://github.com/marcel/aws-s3", response['Location']
+  end
+
+  def test_god
+    response = request "http://god.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "http://godrb.com", response['Location']
+  end
+
+  def test_juggernaut
+    response = request "http://juggernaut.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://blog.alexmaccaw.com/killing-a-library/", response['Location']
+  end
+
+  def test_webgen
+    response = request "http://webgen.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://webgen.gettalong.org", response['Location']
+  end
+
+  def test_maruku
+    response = request "http://maruku.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://benhollis.net/blog/2013/10/20/maruku-is-obsolete/", response['Location']
+  end
+
+  def test_mechanize
+    response = request "http://mechanize.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://www.rubydoc.info/gems/mechanize/", response['Location']
+  end
+
+  def test_rmagick
+    response = request "http://rmagick.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://rmagick.github.io", response['Location']
+  end
+
+  def test_kramdown
+    response = request "http://kramdown.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://kramdown.gettalong.org", response['Location']
+  end
+
+  def test_backgroundrb
+    response = request "http://backgroundrb.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://github.com/gnufied/backgroundrb", response['Location']
+  end
+
+  def test_wtr
+    response = request "http://wtr.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "http://watir.com", response['Location']
+  end
+
+  def test_geokit
+    response = request "http://geokit.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://github.com/geokit/geokit", response['Location']
+  end
+
+  def test_libxml
+    response = request "http://libxml.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://xml4r.github.io/libxml-ruby/", response['Location']
+  end
+
+  def test_celerity
+    response = request "http://celerity.#{DOMAIN}"
+    assert_equal '301', response.code
+    assert_equal "https://github.com/jarib/celerity", response['Location']
+  end
+
   def test_gems
     response = request "http://gems.#{DOMAIN}"
     assert_equal '301', response.code
